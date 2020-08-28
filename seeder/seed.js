bab821f7ea8facc8ffda009eab2d8d0f4ab0470e
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-28 18:52:27 -0500

    added a budget Seed variable

--- seeder/seed.js
+++ seeder/seed.js
@@ -1,7 +1,35 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 	useNewUrlParser: true,
 	useFindAndModify: false,
 });
+
+let budgetSeed = [
+	{
+		name: "Disney Plus Subscription",
+		value: 30,
+		date: new Date().setDate(new Date().getDate() - 5),
+	},
+	{
+		name: "Pet Grooming",
+		value: 75,
+		date: new Date().setDate(new Date().getDate() - 4),
+	},
+	{
+		name: "Hotel Reservation",
+		value: 700,
+		date: new Date().setDate(new Date().getDate() - 3),
+	},
+	{
+		name: "Disney Park Tickets",
+		value: 400,
+		date: new Date().setDate(new Date().getDate() - 2),
+	},
+	{
+		name: "Southwest Tickets",
+		value: 900,
+		date: new Date().setDate(new Date().getDate() - 1),
+	},
+];
